26f58e0b51f3f06b4c2571e736fdfc7fa4273b1a
Mateo Sokac <mxs3203@g.rit.edu>
2023-02-12 16:21:14 +0100

    Add files via upload
    changed code so LSR object has only single instance for all communication(dashboard-lsr_serial). Works with temp. We are making excel with two experiments 24-12 and 12 to 28 C. This will be uploaded later

--- main.py
+++ main.py
@@ -1,24 +1,175 @@
 import json
 import os
 import shutil
+import time
 
+import numpy as np
+import pygad
 from flask import Flask, render_template, request, flash, redirect, url_for, jsonify
 import sqlite3
 
-from werkzeug.utils import secure_filename
+from matplotlib import pyplot as plt
 
 import admin
 
-from LSR.GeneticAlg import GeneticAlg
-from LSR.LSR_comm import LSR_comm
-from LSR.utils import readAndCurateCurve, findLSRTenNumberRange, computeRange
 
+from LSR.LSR_comm import LSR_comm
+from LSR.SpectraWizSaver import save_curve
+from LSR.utils import readAndCurateCurve, findLSRTenNumberRange, computeRange, generate_random, scale_curve
 
 if not admin.isUserAdmin():
     admin.runAsAdmin()
 
 device_port = "COM3"
 app = Flask(__name__)
+lsr = LSR_comm(device_port)
+lsr.stop()
+
+
+def make_plot(ten_nums, recon_curve, ref_curve, nm):
+    plt.plot(nm, recon_curve)
+    plt.plot(nm, ref_curve)
+    plt.legend(['Recreated by {} gen'.format(10), 'Ref Curve'])
+    plt.title("_".join(str(e) for e in ten_nums))
+    plt.savefig("tmp/fig.png", transparent=True)
+
+def on_generation(ga_instance):
+    print("ON GENERATION")
+    with open('tmp/solution_curve.json') as json_file:
+        recon_curve = json.load(json_file)
+        json_file.close()
+    ref_curve, _ = readAndCurateCurve("tmp/ref.IRR")
+    # print(recon_curve)
+    print("\t\tGeneration : ", ga_instance.generations_completed)
+    print("\t\tFitness of the best solution :", ga_instance.best_solution()[1])
+    print("\t\tTen Nums:", ga_instance.best_solution()[0])
+    solution_json = {"generation": ga_instance.generations_completed,
+                     "fitness": ga_instance.best_solution()[1],
+                     "solution": ga_instance.best_solution()[0].tolist(),
+                     "reconstruced_curve": recon_curve[0],
+                     "ref_curve": ref_curve['value'].values.tolist(),
+                     "nm": ref_curve['nm'].values.tolist(),
+                     "temp": recon_curve[1],
+                     "current_process": recon_curve[2],
+                     "tec_status": recon_curve[3]
+                     }
+    solution_json = json.dumps(solution_json)
+    with open("tmp/solution.json", "w") as outfile:
+        outfile.write(solution_json)
+        outfile.close()
+
+    if ga_instance.generations_completed == 10:
+        make_plot(ga_instance.best_solution()[0], recon_curve[0], ref_curve['value'].values.tolist(), ref_curve['nm'].values.tolist())
+
+def fitness_func_offline(solution, soulution_idx):
+    sensor_reading = np.random.randint(120, size=161)
+    sensor_reading_json = sensor_reading.tolist()
+    sensor_reading_json = json.dumps(sensor_reading_json)
+    with open("tmp/solution_curve.json", "w") as outfile:
+        outfile.write(sensor_reading_json)
+        outfile.close()
+
+    desired_output, _ = readAndCurateCurve("tmp/ref.IRR")
+    mse = (np.abs(scale_curve(desired_output['value'].values) - scale_curve(sensor_reading))).mean(axis=0)
+    print("MSE= ", mse)
+    print("Fitness: ", 1.0 / mse)
+    return 1.0 / mse
+
+def fitness_func_online(solution, soulution_idx):
+    solution = [int(ele) for ele in solution]
+    #lsr = LSR_comm("COM3")
+    # Start LSR with params
+    lsr.set_column_data(1, solution)
+    lsr.set_column_data(2, lsr.compute_column_based_on_first(0.7))
+    lsr.set_column_data(3, lsr.compute_column_based_on_first(0.5))
+    lsr.set_column_data(4, lsr.compute_column_based_on_first(0.3))
+    lsr.run()
+
+    lsr.ask_for_status()
+    temp = lsr.block_temp
+    current_process = lsr.current_process
+    tec_status = lsr.tec_status
+
+    # Spectra has to point to example_database folder before starting
+    save_curve("{}".format("recreated.IRR"))
+    print("Waiting for recreated file to be saved...")
+
+    while not os.path.exists("tmp/{}".format("recreated.IRR")):
+        time.sleep(0.2)
+
+    print("\t Reading new HyperOCR data...")
+    # Read HYperOCR (Current Curve)
+    sensor_reading, _ = readAndCurateCurve("tmp/recreated.IRR")
+
+    #sensor_reading = pd.DataFrame(list(zip(np.random.randint(120, size=161),np.random.randint(120, size=161))), columns=['nm','value'])
+    sensor_reading_json = sensor_reading['value'].values.tolist()
+    sensor_reading_json = json.dumps([sensor_reading_json, temp, current_process, tec_status])
+    with open("tmp/solution_curve.json", "w") as outfile:
+        outfile.write(sensor_reading_json)
+        outfile.close()
+
+    desired_output, _ = readAndCurateCurve("tmp/ref.IRR")
+    mse = (np.abs(scale_curve(desired_output['value'].values) - scale_curve(sensor_reading['value'].values))).mean(axis=0)
+    print("MSE= ", mse)
+    print("Fitness: ", 1.0 / mse)
+    return 1.0 / mse
+
+class GeneticAlg:
+
+    def __init__(self, init_range_low, init_range_high, gene_space):
+        self.num_generations = 10
+        self.num_parents_mating = 4
+        self.sol_per_pop = 8
+        self.num_genes = 10
+        self.parent_selection_type = "sss"
+        self.keep_parents = 2
+        self.crossover_type = "scattered"
+        self.mutation_type = "random"
+        self.mutation_percent_genes = 20
+        self.init_range_low = init_range_low
+        self.init_range_high = init_range_high
+        self.gene_space = gene_space
+        self.function_inputs = generate_random(int(init_range_high))
+        self.ga_instance = pygad.GA(num_generations=self.num_generations,
+                                    num_parents_mating=self.num_parents_mating,
+                                    fitness_func=fitness_func_online,
+                                    sol_per_pop=self.sol_per_pop,
+                                    num_genes=self.num_genes,
+                                    gene_type=int,
+                                    gene_space=None,
+                                    init_range_low=self.init_range_low,
+                                    init_range_high=self.init_range_high,
+                                    parent_selection_type=self.parent_selection_type,
+                                    keep_parents=self.keep_parents,
+                                    crossover_type=self.crossover_type,
+                                    mutation_type=self.mutation_type,
+                                    mutation_percent_genes=self.mutation_percent_genes,
+                                    keep_elitism=1,
+                                    save_best_solutions=True,
+                                    on_generation=on_generation)
+
+
+    def run(self):
+        self.ga_instance.run()
+        print(self.ga_instance.best_solutions)
+        solution, solution_fitness, solution_idx = self.ga_instance.best_solution()
+        print("Parameters of the best solution : {solution}".format(solution=solution))
+        print("Fitness value of the best solution = {solution_fitness}".format(solution_fitness=solution_fitness))
+
+        prediction = np.sum(np.array(self.function_inputs) * solution)
+        print("Predicted output based on the best solution : {prediction}".format(prediction=prediction))
+
+
+###############################################################
+###############################################################
+###############################################################
+###############################################################
+###############################################################
+###############################################################
+###############################################################
+###############################################################
+###############################################################
+
 
 @app.route('/')
 def dashboard():
@@ -41,6 +192,7 @@ def figure():
         return render_template('figure.html', figurePath=figurePath)
     else:
         return "{}", 404
+
 @app.route('/create', methods=('GET', 'POST'))
 def create():
 
@@ -90,26 +242,11 @@ def clearTmp():
         os.remove("tmp/fig.png")
     if os.path.isfile("tmp/recreated.IRR"):
         os.remove("tmp/recreated.IRR")
-@app.route('/set_lsr_temp')
-def set_lsr_temp():
-    if request.method == 'GET':
-        task = request.args.get('task')
-        value = request.args.get('value')
-        print(task, value)
-
-        lsr = LSR_comm(device_port)
-        lsr.set_block_temp(value)
-        lsr.run()
-
-        return "{}", 200
-    else:
-        return "{}", 205
 
 @app.route('/abort_lsr')
 def abort_lsr():
     if request.method == 'GET':
 
-        lsr = LSR_comm(device_port)
         lsr.stop()
 
         return "{}", 200
@@ -119,6 +256,7 @@ def abort_lsr():
 @app.route('/findCurve',methods=['POST'])
 def findCurve():
     if request.method == 'POST':
+
         f = request.files['file']
         f.save("tmp/ref.IRR")
         curve, log10curve = readAndCurateCurve("tmp/ref.IRR")
@@ -151,16 +289,32 @@ def get_current_solution():
 @app.route('/get_status')
 def get_status():
     if request.method == 'GET':
-        lsr = LSR_comm(device_port)
+        #lsr = LSR_comm(device_port)
         lsr.ask_for_status()
         data = {}
         data["temp"] = lsr.block_temp
         data["current_process"] = lsr.current_process
         data["tec_status"] = lsr.tec_status
         data["connected"] = True
+
         return data, 200
     else:
         return "", 404
+
+@app.route('/set_lsr_temp')
+def set_lsr_temp():
+    if request.method == 'GET':
+        task = request.args.get('task')
+        value = request.args.get('value')
+        if value != None:
+            value = float(value) * 10.0
+            #22.6, 226
+            lsr.set_block_temp(int(value))
+
+        return "{}", 200
+    else:
+        return "{}", 205
+
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
@@ -169,5 +323,8 @@ def get_db_connection():
 
 if __name__ == "__main__":
     clearTmp()
-    app.debug = True
+    app.debug = False
     app.run()
+
+
+
